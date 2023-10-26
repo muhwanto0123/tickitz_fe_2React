257e4e44569c6c91d21195905d4beda786d47157
muhwanto0123 <55321001+muhwanto0123@users.noreply.github.com>
2023-10-26 16:41:58 +0700

    [UPDATE]
    update login dan register

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,94 @@
+import "../style/Auth.css";
+import "../style/Auth.mobile.css";
+
+import { Link } from "react-router-dom";
 import React from "react";
-import axios from "axios";
-import { useParams } from "react-router-dom";
 
-import "../style/Detail.css";
-import "../style/Detail.mobile.css"
-
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-
-function login_page() {
-    return (
-        <>
-        
-        </>
-    )
+function Login() {
+  return (
+    <div id="page_login" style={{ overflow: "hidden" }}>
+      <div className="row">
+        <div className="col col-md-7">
+          <div
+            style={{
+              width: "100%",
+              height: "100vh",
+              backgroundImage: "url('/img/background_img/background.jpg')",
+              backgroundSize: "cover",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+            }}
+          >
+            <Link to="/">
+                <img
+                src="/img/icon_media/tickitz_logo.png"
+                width="500px"
+                height="190px"
+                />
+            </Link>
+            <p style={{ fontSize: "35px", color: "white" }}>
+              Wait, Watch, Wow!
+            </p>
+          </div>
+        </div>
+        <div className="col col-md-5">
+          <div
+            className="pe-5 p-2"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              height: "100vh",
+            }}
+          >
+            <div style={{ width: "100%" }}>
+              <h1 style={{ fontSize: "48px" }}>Sign In</h1>
+              <p
+                style={{
+                  fontSize: "18px",
+                  color: "grey",
+                  marginBottom: "30px",
+                }}
+              >
+                Sign in with your data that you 
+                entered during your registration
+              </p>
+              <div className="mb-3">
+                <label className="form-label">Email</label>
+                <input
+                  className="form-control form-control-lg"
+                  placeholder="write your email"
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Password</label>
+                <input
+                  className="form-control form-control-lg"
+                  placeholder="write your password"
+                />
+              </div>
+              <div className="d-grid pt-2">
+                <button className="btn btn-primary btn-lg">Sign In</button>
+              </div>
+              <p className="text-center pt-4">
+                Forgot your password ? {" "}
+                <Link to="/forgot-account" style={{ color: "#5F2EEA" }}>
+                    Reset Now
+                </Link>
+              </p>
+              <p className="text-center">
+                Don't have an account ? {" "}
+                <Link to="/register" style={{ color: "#5F2EEA" }}>
+                    Sign Up
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default login_page;
+export default Login;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,11 +1,136 @@
-import React from "react";
+import "../style/Auth.css";
+import "../style/Auth.mobile.css";
 
-function Register () {
-    return (
-        <>
-        
-        </>
-    )
+import { Link } from "react-router-dom";
+import React from "react";
+import axios from "axios";
+
+function Register() {
+  const [fullname, setFullName] = React.useState("");
+  const [phoneNumber, setPhoneNumber] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleRefister = () => {
+    axios
+        .post("https://tickitz-be.onrender.com/v1/auth/register", {
+            email: email,
+            password: password,
+            fullname: fullname,
+            phone_number: phoneNumber,
+        })
+        .then(() => {
+            console.log("berhasil")
+        }).catch(() => {
+            console.log("error")
+        })
+  };
+
+  return (
+    <div id="page_register" style={{ overflow: "hidden" }}>
+      <div className="row">
+        <div className="col col-md-7">
+          <div
+            style={{
+              width: "100%",
+              height: "100vh",
+              backgroundImage: "url('/img/background_img/background.jpg')",
+              backgroundSize: "cover",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+            }}
+          >
+            <Link to="/">
+              <img
+                src="/img/icon_media/tickitz_logo.png"
+                width="500px"
+                height="190px"
+              />
+            </Link>
+            <p style={{ fontSize: "35px", color: "white" }}>
+              Wait, Watch, Wow!
+            </p>
+          </div>
+        </div>
+        <div className="col col-md-5">
+          <div
+            className="pe-5 p-2"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              height: "100vh",
+            }}
+          >
+            <div className="pt-2" style={{ width: "100%" }}>
+              <h1 style={{ fontSize: "35px" }}>Sign-Up</h1>
+              <p
+                style={{
+                  fontSize: "18px",
+                  color: "grey",
+                  marginBottom: "30px",
+                }}
+              >
+                Fill your additional details
+              </p>
+
+              <div className="mb-2">
+                <label className="form-label">Full Name</label>
+                <input
+                  className="form-control form-control-lg"
+                  placeholder="write your full Name"
+                  onChange={(event) => {
+                    setFullName(event.target.value);
+                  }}
+                />
+              </div>
+              <div className="mb-2">
+                <label className="form-label">Phone Number</label>
+                <input
+                  className="form-control form-control-lg"
+                  placeholder="write your phone number"
+                  onChange={(event) => {
+                    setPhoneNumber(event.target.value);
+                  }}
+                />
+              </div>
+              <div className="mb-2">
+                <label className="form-label">Email</label>
+                <input
+                  className="form-control form-control-lg"
+                  placeholder="write your email"
+                  onChange={(event) => {
+                    setEmail(event.target.value);
+                  }}
+                />
+              </div>
+              <div className="mb-2">
+                <label className="form-label">Password</label>
+                <input
+                  className="form-control form-control-lg"
+                  placeholder="write your password"
+                  type="password"
+                  onChange={(event) => {
+                    setPassword(event.target.value);
+                  }}
+                />
+              </div>
+              <div className="d-grid pt-4">
+                <button className="btn btn-primary btn-lg">Sign Up</button>
+              </div>
+              <p className="text-center pt-4">
+                Alreadt have account ?{" "}
+                <Link to="/login" style={{ color: "#5F2EEA" }}>
+                  Sign-in
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
